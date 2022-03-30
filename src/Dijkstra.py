9213657129d1de6778ed592d775eea2db959075a
Matt Umut Ucak <umutucak@protonmail.com>
2022-03-30 18:19:17 +0200

    Dijkstra fixed

--- src/Dijkstra.py
+++ src/Dijkstra.py
@@ -1,4 +1,5 @@
 from queue import PriorityQueue
+import sys
 
 def search(start, goal, graph):
 	priority_queue = PriorityQueue(0)
@@ -7,14 +8,15 @@ def search(start, goal, graph):
 	priority_queue.put((0, start))
 	while not priority_queue.empty():
 		current = priority_queue.get()
+		visited[current[1]] = True
 		if current[1] == goal:
 			return backtrack(start, current[1], backtrack_table)
 		else:
 			children = get_children(graph, current, visited)
 			for child in children:
 				g_score = child[0] + current[0]
+				# print(child[1], '->', g_score)
 				priority_queue.put((g_score, child[1]))
-				visited[child[1]] = True
 				backtrack_table[child[1]] = current[1]
 	return []
 
