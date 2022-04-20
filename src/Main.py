f52b2c2de45abffc1e444b5c0622959df15371dc
TitouanUniLu <titouan.guerin.001@student.uni.lu>
2022-04-20 10:51:03 +0200

    added space

--- src/Main.py
+++ src/Main.py
@@ -7,20 +7,20 @@ start_time = time.time()
 DFS_data = DFS.search(start, goal, Graph.map)
 DFS_time = time.time() - start_time
 print("DFS Path: " + str(DFS_data[0]) + " | Execution Time: " + str(DFS_time) +  " | Path Length: " + str(len(DFS_data[0])) + " | Total Expanded Nodes: " + str(DFS_data[1]))
-print("DFS complexity: O(V) + O(E) where E is the number of edges, and V is the number of vertices.")
+print("DFS complexity: O(V) + O(E) where E is the number of edges, and V is the number of vertices.\n")
 
 start_time = time.time()
 BFS_data = BFS.search(start, goal, Graph.map)
 BFS_time = time.time() - start_time
 print("BFS Path: " + str(BFS_data[0]) + " | Execution Time: " + str(BFS_time) +  " | Path Length: " + str(len(BFS_data[0])) + " | Total Expanded Nodes: " + str(BFS_data[1]))
-print("BFS complexity: O(V) + O(E) where E is the number of edges, and V is the number of vertices.")
+print("BFS complexity: O(V) + O(E) where E is the number of edges, and V is the number of vertices.\n")
 
 
 start_time = time.time()
 Dijkstra_data = Dijkstra.search(start, goal, Graph.map)
 Dijkstra_time = time.time() - start_time
 print("Dijkstra Path: " + str(Dijkstra_data[0]) + " | Execution Time: " + str(Dijkstra_time) +  " | Path Length: " + str(len(Dijkstra_data[0])) + " | Total Expanded Nodes: " + str(Dijkstra_data[1]))
-print("Dijkstra complexity: O(V^2) where V is the amount of vertices")
+print("Dijkstra complexity: O(V^2) where V is the amount of vertices\n")
 
 start_time = time.time()
 euclidean = lambda sld, city: sld[city]
